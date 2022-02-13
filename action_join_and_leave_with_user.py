b4983ba31a245fa7c1a6fba8dd3a1aafdf9e94a0
Parsa Torbati <ptorbatii@gmail.com>
2022-02-13 12:25:59 +0330

    restart the timer even if there was an error

--- action_join_and_leave_with_user.py
+++ action_join_and_leave_with_user.py
@@ -16,58 +16,61 @@ def check(info):
 
 
 def callback(info, timer, join_url):
-    timer.stop()
-    beep()
-    print("Join URL:")
-    print(join_url)
-    import time
-    print(f"Joining meeting automatically. t={time.time()}")
-    target_user_name = None
-    for attendee in info["info"]["attendees"]["attendee"]:
-        if attendee["userID"] == action_join_and_leave_with_user["user_id"]:
-            target_user_name = attendee["fullName"]
-    if target_user_name is None:
+    try:
+        timer.stop()
+        beep()
+        print("Join URL:")
+        print(join_url)
+        import time
+        print(f"Joining meeting automatically. t={time.time()}")
+        target_user_name = None
+        for attendee in info["info"]["attendees"]["attendee"]:
+            if attendee["userID"] == \
+                    action_join_and_leave_with_user["user_id"]:
+                target_user_name = attendee["fullName"]
+        if target_user_name is None:
+            timer.start()
+            print("ERROR: callback called but target user is not present.")
+            return
+        from selenium.webdriver import Firefox
+        from selenium.webdriver.support.ui import WebDriverWait
+        from selenium.webdriver.support import expected_conditions as EC
+        from selenium.webdriver.common.by import By
+        from selenium.common.exceptions import TimeoutException
+        firefox = Firefox()
+        firefox.implicitly_wait(5)
+        firefox.get(join_url)
+        try:
+            WebDriverWait(firefox, 30).until(
+                EC.element_to_be_clickable(
+                    (By.CSS_SELECTOR, ".icon-bbb-listen")
+                )
+            ).click()
+        except TimeoutException:
+            print("cannot join audio conference: element not found.")
+        try:
+            WebDriverWait(firefox, 20).until(
+                EC.element_to_be_clickable(
+                    (By.CSS_SELECTOR, ".icon-bbb-thumbs_up")
+                )
+            ).click()
+        except TimeoutException:
+            pass
+        firefox.implicitly_wait(5)
+        for _ in range(16):
+            WebDriverWait(firefox, 60 * 90).until_not(
+                EC.presence_of_element_located(
+                    (By.XPATH,
+                     f"//*[text()='{target_user_name}']"
+                     )
+                )
+            )
+            time.sleep(.5)
+        print("Target user has left, enabling watchdog and leaving.",
+              f"t={time.time()}")
+        firefox.quit()
+    finally:
         timer.start()
-        print("ERROR: callback called but target user is not present.")
-        return
-    from selenium.webdriver import Firefox
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    from selenium.webdriver.common.by import By
-    from selenium.common.exceptions import TimeoutException
-    firefox = Firefox()
-    firefox.implicitly_wait(5)
-    firefox.get(join_url)
-    try:
-        WebDriverWait(firefox, 30).until(
-            EC.element_to_be_clickable(
-                (By.CSS_SELECTOR, ".icon-bbb-listen")
-            )
-        ).click()
-    except TimeoutException:
-        print("cannot join audio conference: element not found.")
-    try:
-        WebDriverWait(firefox, 20).until(
-            EC.element_to_be_clickable(
-                (By.CSS_SELECTOR, ".icon-bbb-thumbs_up")
-            )
-        ).click()
-    except TimeoutException:
-        pass
-    firefox.implicitly_wait(5)
-    for _ in range(16):
-        WebDriverWait(firefox, 60 * 90).until_not(
-            EC.presence_of_element_located(
-                (By.XPATH,
-                 f"//*[text()='{target_user_name}']"
-                 )
-            )
-        )
-        time.sleep(.5)
-    print("Target user has left, enabling watchdog and leaving.",
-          f"t={time.time()}")
-    firefox.quit()
-    timer.start()
 
 
 Watchdog(
